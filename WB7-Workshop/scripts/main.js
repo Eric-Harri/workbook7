1d7b05239e529fb2a66943ed3e3b87e97e365246
Eric Harris <ericoldie@gmail.com>
2022-11-10 13:55:16 -0500

    working code

--- WB7-Workshop/scripts/main.js
+++ WB7-Workshop/scripts/main.js
@@ -1,25 +1,47 @@
-"strict use"
+"strict use";
 
-const selectDropdown = document.getElementById("select-dropdown")
-const categoryDropdown = document.getElementById("category-dropdown")
+const selectDropdown = document.getElementById("select-dropdown");
+const categoryDropdown = document.getElementById("category-dropdown");
+const productTableBody = document.querySelector("#product-table tbody");
 
-categoryDropdown.style.display = none;
+categoryDropdown.style.display = "none";
 
 function loadCategoryDropdown() {
-    selectedValue = selectDropdown.value
+  selectedValue = selectDropdown.value;
 
-        fetch("http://localhost:8081/api/categories")
-            .then((response) => response.json())
-            .then((categories) => {
-                if (selectedValue === all) {
-
-                }
-        });
+  fetch("http://localhost:8081/api/products")
+    .then((response) => response.json())
+    .then((products) => {
+      products.forEach((product) => {
+        if (selectedValue === "all") {
+          buildTable(productTableBody, product);
+        }
+      });
+    });
 }
 
+function buildTable(tablebody, product) {
+  let row = tablebody.insertRow(-1);
 
+  let cell1 = row.insertCell(0);
+  cell1.innerText = product.productId;
 
+  let cell2 = row.insertCell(1);
+  cell2.innerText = product.productName;
+
+  let cell3 = row.insertCell(2);
+  cell3.innerText = Number(product.unitPrice).toFixed(2);
+  
+
+  let cell4 = row.insertCell(3);
+    const a = document.createElement("a");
+    a.innerText = "see details";
+    a.href = `./product-details.html?productid=${product.productId}`;
+    a.target = "_blank";
+    cell4.appendChild(a);
+  
+}
 
 window.onload = () => {
-    selectDropdown.onchange = loadCategoryDropdown;
-}
+  selectDropdown.onchange = loadCategoryDropdown;
+};
