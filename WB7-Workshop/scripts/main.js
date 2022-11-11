c0f0cb4bcab5b78ba318d9bf0e85122f5fcfd283
Eric Harris <ericoldie@gmail.com>
2022-11-11 10:43:24 -0500

    added code

--- WB7-Workshop/scripts/main.js
+++ WB7-Workshop/scripts/main.js
@@ -14,33 +14,28 @@ function loadCategoryDropdown() {
     .then((products) => {
       products.forEach((product) => {
         if (selectedValue === "all") {
-          buildTable(productTableBody, product);
+          let row = productTableBody.insertRow(-1);
+          let cell1 = row.insertCell(0);
+          cell1.innerText = product.productId;
+
+          let cell2 = row.insertCell(1);
+          cell2.innerText = product.productName;
+
+          let cell3 = row.insertCell(2);
+          cell3.innerText = Number(product.unitPrice).toFixed(2);
+
+          let cell4 = row.insertCell(3);
+          const a = document.createElement("a");
+          a.innerText = "See Details";
+          a.href = `./product-details.html?productid=${product.productId}`;
+          a.target = "_blank";
+          cell4.appendChild(a);
         }
       });
     });
 }
 
-function buildTable(tablebody, product) {
-  let row = tablebody.insertRow(-1);
 
-  let cell1 = row.insertCell(0);
-  cell1.innerText = product.productId;
-
-  let cell2 = row.insertCell(1);
-  cell2.innerText = product.productName;
-
-  let cell3 = row.insertCell(2);
-  cell3.innerText = Number(product.unitPrice).toFixed(2);
-  
-
-  let cell4 = row.insertCell(3);
-    const a = document.createElement("a");
-    a.innerText = "see details";
-    a.href = `./product-details.html?productid=${product.productId}`;
-    a.target = "_blank";
-    cell4.appendChild(a);
-  
-}
 
 window.onload = () => {
   selectDropdown.onchange = loadCategoryDropdown;
